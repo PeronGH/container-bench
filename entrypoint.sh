9990e7e8e8d2eca3702b79503639616018300fb3
Peron <peron_ap@icloud.com>
2026-06-11 12:56:14 +0800

    refactor: track benchmark progress with counter and total variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 set -eu
 
-echo "(1/3) running 7zip LZMA"
+TOTAL=3
+i=0
+
+step() {
+    i=$((i + 1))
+    echo "($i/$TOTAL) running $1"
+}
+
+step "7zip LZMA"
 7z b -mtime=7
-echo "(2/3) running 7zip SHA256"
+step "7zip SHA256"
 7z b -mtimems=3300 -mm=SHA256
-echo "(3/3) running 7zip AES256CBC"
+step "7zip AES256CBC"
 7z b -mtimems=3300 -mm=AES256CBC
