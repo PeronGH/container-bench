72a5339c655720da7756fa7405a7d910089e6267
Peron <peron_ap@icloud.com>
2026-06-11 12:55:54 +0800

    feat: time-cap each benchmark to roughly one minute

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -eu
 
 echo "(1/3) running 7zip LZMA"
-7z b
+7z b -mtime=7
 echo "(2/3) running 7zip SHA256"
-7z b -mm=SHA256
+7z b -mtimems=3300 -mm=SHA256
 echo "(3/3) running 7zip AES256CBC"
-7z b -mm=AES256CBC
+7z b -mtimems=3300 -mm=AES256CBC
